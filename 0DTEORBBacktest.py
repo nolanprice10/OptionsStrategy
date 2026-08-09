3cef965a542d1fe89b86dbcdf738224793acbe3d
nolanprice10 <nolangp10@gmail.com>
2026-08-09 14:07:14 -0400

    add additional trading parameters in 0DTEORBBacktest.py

--- 0DTEORBBacktest.py
+++ 0DTEORBBacktest.py
@@ -5,3 +5,12 @@ ticker = 'SPY'
 interval = '5m'
 period = '60d'
 
+or_minutes = 15
+or_bars = or_minutes // 5
+
+stop_loss = 0.5
+take_profit = 0.5
+leverage = 8.0
+
+starting_balance = 100
+risk_per_trade = 0.2
